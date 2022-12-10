39066dccc419532fb703f04aa8909cee06391b82
Efa-Cb <Efacb001@gmail.com>
2022-12-09 16:31:15 -0800

    print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints  alphabets in lowercase
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
+
