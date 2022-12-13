7f4384d43f98eee39bd7afc24d638b882f37beaf
Efa-Cb <Efacb001@gmail.com>
2022-12-13 09:52:13 -0800

    header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchari(char c)
+int _putchari(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -12,6 +12,6 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int
+void print_to_98(int)
 
 #endif
