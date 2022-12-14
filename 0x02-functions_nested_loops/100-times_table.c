530fe4219574f53f5b5781e6f77f246b8444038f
Efa-Cb <Efacb001@gmail.com>
2022-12-13 21:01:06 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,51 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
+ * print_times_table - prints time table with provided number
+ * @n: the provided value by the user
+ * Return: Always 0 (Success)
  */
 
 void print_times_table(int n)
 {
-	int i, j, k;
+	int row;
+	int column;
+	int product;
 
-	if (n >= 0 && n <= 15)
+	if (n < 0 || n >= 15)
+		return;
+	for (row = 0; row <= n; row++)
 	{
-		for (i = 0; i <= n; i++)
+		for (column = 0; column <= n; column++)
 		{
-			for (j = 0; j <= n; j++)
+			product = (row * column);
+			if (column == 0)
+				_putchar('0' + product);
+			else
 			{
-				k = j * i;
-				if (j == 0)
+				_putchar(',');
+				_putchar(' ');
+				if (product <= 9)
 				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + product);
+				}
+				else if (product > 9 && product < 100)
 				{
-					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
+					_putchar('0' + (product / 10));
+					_putchar('0' + (product % 10));
+				}
+				else if (product >= 100)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
+					_putchar('0' + (product / 100));
+					_putchar('0' + ((product / 10) % 10));
+					_putchar('0' + (product % 10));
 				}
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
+}
