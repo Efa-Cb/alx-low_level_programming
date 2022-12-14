560a7fd3dff4c3e84a299c25d81ba45c9e2d5200
Efa-Cb <Efacb001@gmail.com>
2022-12-13 20:34:11 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,25 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print a - z ten times
- *
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
  */
-
 void print_alphabet_x10(void)
-
 {
+	char ch;
 	int i;
-	char j;
 
-	for (i = 0; i < 10; i++)
+	i = 0;
+
+	while (i < 10)
 	{
-		for (j = 'a'; j <= 'z'; j++)
+		ch = 'a';
+		while (ch <= 'z')
 		{
-			_putchar(j);
+			_putchar(ch);
+			ch++;
 		}
-
-		putchar('\n');
+		_putchar('\n');
+		i++;
 	}
-
 }
