f09e86e725ae6dfc2ed630e12f3e3ff7946263af
Efa-Cb <Efacb001@gmail.com>
2022-12-19 09:23:13 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string -  function reverses a string
+ * @s: parameter of pointer
+ *
+ *Return: Always 0 (Success)
+ */
+
+void rev_string(char *s)
+{
+	int len, i, half;
+	char temp;
+
+	for (len = 0; s[len] != '\0'; len++)
+	;
+	i = 0;
+	half = len / 2;
+
+	while (half--)
+	{
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
+		i++;
+	}
+}
