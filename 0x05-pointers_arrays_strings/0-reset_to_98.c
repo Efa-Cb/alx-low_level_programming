1e52c294882efdb779f6c5081a1204807af2e02f
Efa-Cb <Efacb001@gmail.com>
2022-12-19 08:15:03 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - update value of pointer  to 98
+ * @n: parameter to be provided
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
