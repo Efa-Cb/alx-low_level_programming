4f08e49d96a192b093f93183d731a6685c5c7588
Efa-Cb <Efacb001@gmail.com>
2022-12-22 05:55:49 -0800

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - function Prints an integer.
+ * @n: The integer to be printed.
+ * Return: Always 0
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+}
+
+
