8e8ff134e3a180eb91a6dcb1b0038485aee62f5f
Efa-Cb <Efacb001@gmail.com>
2023-01-10 09:16:14 -0800

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup - Function copies the string given as parameter.
+ * @str: string to be  duplicate
+ *
+ * Return: pointer to the copied string (Success), NULL (Error)
+ */
+
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int i, len;
+
+	i = 0;
+	len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	dup = malloc(sizeof(char) * (len + 1));
+
+	if (dup == NULL)
+		return (NULL);
+
+	while ((dup[i] = str[i]) != '\0')
+		i++;
+
+	i++;
+
+	return (dup);
+}
