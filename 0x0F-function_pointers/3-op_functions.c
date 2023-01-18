38eff2062eb40576a78631378f9474f29bfd6a82
Efa-Cb <Efacb001@gmail.com>
2023-01-18 14:06:30 -0800

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,62 +1,71 @@
-#include "3-calc.h"
-
 /**
- * op_add - calculates the sum of two integers
- * @a: first integer
- * @b: second integer
+ * op_add - adds @a and @b
  *
- * Return: sum of a and b
- */
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: sum of @a and @b
+*/
+
 int op_add(int a, int b)
 {
+
 	return (a + b);
 }
 
+
 /**
- * op_sub - calculates the difference of two integers
- * @a: first integer
- * @b: second integer
+ * op_sub - gets the difference of @a and @b
  *
- * Return: difference of a and b
- */
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: difference of @a and @b
+*/
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+
 /**
- * op_mul - calculates the product of two integers
- * @a: first integer
- * @b: second integer
+ * op_mul - multiplies @a and @b
  *
- * Return: product of a and b
- */
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: product of @a and @b
+*/
 int op_mul(int a, int b)
 {
+
 	return (a * b);
 }
 
+
 /**
- * op_div - calculates the division of two integers
- * @a: first integer
- * @b: second integer
+ * op_div - divides @a by @b
  *
- * Return: result of the division of a and b
- */
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: results of the division @a and @b
+*/
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 
+
 /**
- * op_mod - calculates the remainder of the division of two integers
- * @a: first integer
- * @b: second integer
+ * op_mod - gets the reminder of the division between @a and @b
  *
- * Return: remainder of a divided b
- */
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: the remainder
+*/
 int op_mod(int a, int b)
 {
-
 	return (a % b);
 }
